c546a958acff263fcd2b22a6f8428d4ee0530b05
ozgend <denizo@armut.com>
2021-03-08 10:24:30 +0300

    added sample aws cli

---
+++ .sample/iac/cli/s3.bat
@@ -0,0 +1,2 @@
+aws --endpoint-url=http://localhost:4566 s3 mb s3://denolk-hive-cli
+aws --endpoint-url=http://localhost:4566 s3 ls
---
+++ .sample/iac/cli/s3.sh
@@ -0,0 +1,4 @@
+#! bin/bash
+
+aws --endpoint-url=http://localhost:4566 s3 mb s3://denolk-hive-cli
+aws --endpoint-url=http://localhost:4566 s3 ls
