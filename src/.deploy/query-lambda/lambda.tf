7ebdaf10d0d449d47dd952dbaf189bb884cbee4e
ozgend <revolter00@gmail.com>
2021-03-10 01:51:13 +0300

    tf s3 lambda upload

--- src/.deploy/query-lambda/lambda.tf
+++ src/.deploy/query-lambda/lambda.tf
@@ -1,6 +1,8 @@
-data "aws_s3_bucket_object" "query_lambda_pack" {
+resource "aws_s3_bucket_object" "query_lambda_pack" {
   bucket = var.lambda_bucket
   key    = "query-lambda/${var.lambda_version}/lambda.zip"
+  source = "../../.dist/query-lambda/${var.lambda_version}/lambda.zip"
+  etag   = filemd5("../../.dist/query-lambda/${var.lambda_version}/lambda.zip")
 }
 
 data "aws_iam_role" "lambda_role" {
@@ -8,10 +10,14 @@ data "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_lambda_function" "query_lambda" {
-  function_name    = "query-lambda"
-  s3_bucket        = data.aws_s3_bucket_object.query_lambda_pack.bucket
-  s3_key           = data.aws_s3_bucket_object.query_lambda_pack.key
-  handler          = "index.handler"
-  runtime          = "nodejs12.x"
-  role             = data.aws_iam_role.lambda_role.arn
+  function_name = "query-lambda"
+  s3_bucket     = aws_s3_bucket_object.query_lambda_pack.bucket
+  s3_key        = aws_s3_bucket_object.query_lambda_pack.key
+  handler       = "index.handler"
+  runtime       = "nodejs12.x"
+  role          = data.aws_iam_role.lambda_role.arn
+}
+
+output "lambda_zip" {
+  value = aws_s3_bucket_object.query_lambda_pack.key
 }
--- src/pack.sh
+++ src/pack.sh
@@ -6,8 +6,9 @@ export AWS_DEFAULT_REGION=us-east-1
 
 APP=$1
 VERSION=$2
-LAMBDA_BUCKET=denolk-lambda-storage
+# LAMBDA_BUCKET=denolk-lambda-storage
 
-mkdir  ./.dist/$APP/$VERSION/ -p
+mkdir -p ./.dist/$APP/$VERSION/
 (cd $APP && zip -r -D ../.dist/$APP/$VERSION/lambda.zip * -x '*node_modules*')
-aws --endpoint-url=http://localhost:4566 s3 cp ./.dist/$APP/$VERSION/lambda.zip s3://$LAMBDA_BUCKET/$APP/$VERSION/lambda.zip
+echo "pack completed: $APP/$VERSION/lambda.zip"
+# aws --endpoint-url=http://localhost:4566 s3 cp ./.dist/$APP/$VERSION/lambda.zip s3://$LAMBDA_BUCKET/$APP/$VERSION/lambda.zip
