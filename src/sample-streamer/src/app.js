5f684b3b7ee2ac87a1b602e31c2494da7656f335
ozgend <revolter00@gmail.com>
2021-03-11 11:39:39 +0300

    added sample kinesis streamer

---
+++ src/sample-streamer/src/app.js
@@ -0,0 +1,15 @@
+const { v4: uuid } = require('uuid');
+const EventStreamer = require('./event-streamer');
+const _eventStreamer = new EventStreamer();
+
+_eventStreamer.startCollector();
+
+const streamGenerator = () => {
+  _eventStreamer.add([{
+    hash: uuid(),
+    amount: Math.floor(Math.random() * Math.floor(99)),
+    timestamp: Date.now()
+  }]);
+};
+
+setInterval(streamGenerator, 1000);
---
+++ src/sample-streamer/src/event-streamer.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const _period = 2;
+const _stream = 'transaction-stream';
+const _queue = [];
+
+class EventStreamer {
+  constructor() {
+    this._kinesis = new AWS.Kinesis({ region: 'us-east-1', endpoint: 'http://localhost:4566' });
+  }
+
+  add(items) {
+    _queue.push(...items);
+  }
+
+  startCollector() {
+    setInterval(this._queueCollector, _period, this._kinesis);
+  }
+
+  _queueCollector(kinesis) {
+    if (_queue.length === 0) {
+      return;
+    }
+
+    const items = _queue.splice(0, 100);
+
+    const records = items.map(item => {
+      return {
+        Data: JSON.stringify(item),
+        PartitionKey: `p_${item.timestamp}`
+      };
+    });
+
+    const recordsParams = {
+      Records: records,
+      StreamName: _stream
+    };
+
+    kinesis.putRecords(recordsParams, (err, data) => {
+      if (err) {
+        console.info(`kinesis error: ${err}`);
+      }
+      else {
+        console.info(`>> streamed ${records.length} events | [${items[0].hash}]`);
+      }
+    });
+  }
+};
+
+module.exports = EventStreamer;
