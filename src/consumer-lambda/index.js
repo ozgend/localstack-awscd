699cc84b16b433d43a3a6f4dbfe8f8a6b6b801c4
ozgend <revolter00@gmail.com>
2021-03-11 11:39:07 +0300

    consumer lambda impl.

--- src/consumer-lambda/index.js
+++ src/consumer-lambda/index.js
@@ -1,5 +1,26 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1', credentials: { accessKeyId: 'mock', secretAccessKey: 'mock' } });
+const db = new AWS.DynamoDB({ apiVersion: '2012-08-10', endpoint: 'http://localhost:4566' });
+
 exports.handler = async function (event, context) {
-    console.log('>  consumer-lambda called');
-    console.log(`event: ${JSON.stringify(event, null, 2)}`);
-    return 'consumer-lambda';
+    // console.log('++++++++++++++++++++++  consumer-lambda called');
+    // console.log(`event: ${JSON.stringify(event, null, 2)}`);
+
+    const items = event.Records.map(record => {
+        const transaction = Buffer.from(record.kinesis.data, 'base64').toString('ascii');
+        return {
+            TableName: 'transactions',
+            Item: {
+                'hash': { S: transaction.hash },
+                'amount': { N: `${transaction.amount}` },
+                'timestamp': { N: `${transaction.timestamp}` }
+            }
+        };
+    });
+
+    for (let i = 0; i < items.length; i++) {
+        await db.putItem(items[i]).promise();
+    }
+
+    return `consumed ${items.length} items`;
 };
