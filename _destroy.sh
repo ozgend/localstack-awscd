86b5742ea19ea9719d5640eaf80a62e854645d8d
ozgend <revolter00@gmail.com>
2021-03-13 12:34:06 +0300

    updated scripts

--- _destroy.sh
+++ _destroy.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+echo 'destroying resources'
 (cd src/.deploy/consumer-lambda && terraform destroy -auto-approve)
 (cd src/.deploy/query-lambda && terraform destroy -auto-approve)
 (cd infrastructure && terraform destroy -auto-approve)
-find . -name '*.tfstate*' -delete
+
+echo 'terminating containers'
 (cd localstack-compose && docker-compose kill && docker-compose rm -f)
+
+echo 'removing files'
+find . -name '*.tfstate*' -delete
 rm -rf ./localstack-compose/volume/tmp
 rm -rf ./src/.dist
--- _init.sh
+++ _init.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-# localstack + infra
+echo 'creating localstack and infrastructure'
 (cd localstack-compose && docker-compose up -d)
 (cd infrastructure && terraform init && terraform apply -auto-approve)
 
-## consumer-lambda
+echo 'creating consumer-lambda with resources'
 (cd src && ./pack.sh consumer-lambda v1)
 (cd src/.deploy/consumer-lambda && terraform init && terraform apply -var="lambda_version=v1" -auto-approve)
 
-## query-lambda
+echo 'creating query-lambda with resources'
 (cd src && ./pack.sh query-lambda v1)
 (cd src/.deploy/query-lambda && terraform init && terraform apply -var="lambda_version=v1" -auto-approve)
 
